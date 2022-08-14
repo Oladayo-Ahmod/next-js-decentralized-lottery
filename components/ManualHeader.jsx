edd701ce577234147c35e1be2f72d2a140d9c628
oladayo ahmod <oladayoahmod112@gmail.com>
2022-08-14 21:57:33 +0100

    connection functionality with moralis

--- components/ManualHeader.jsx
+++ components/ManualHeader.jsx
@@ -1,11 +1,25 @@
+import { useEffect } from 'react'
 import {useMoralis} from 'react-moralis'
 
 export default function Header(){
-    const {enableWeb3, account} = useMoralis()
+    const {enableWeb3, account,isWeb3Enabled} = useMoralis()
+    useEffect(()=>{
+        if (isWeb3Enabled) {
+            return
+        }
+        if (localStorage.getItem('connected')) {
+            enableWeb3()
+        }
+    },[isWeb3Enabled])
     return (
         account
-        ? (<div>Connected</div>) 
-        : ( <button onClick={async ()=> {await enableWeb3()}}>Connect</button>)
+        ? (<div> Connected to {account.slice(0,6)}...{account.slice(account.length -4)} </div>) 
+        : ( <button onClick={async ()=> {
+            if (window !== 'undefined') {
+                localStorage.setItem('connected','inject')
+            }
+            await enableWeb3()
+        }}>Connect</button>)
        
     )
 }
